dd24ad3e8531a1f8536846654f377125b7753908
Gihan Marasingha <gihan.marasingha@gmail.com>
2023-10-25 18:39:51 +0100

    update requirements.txt to allow a range of pypdf versions use pypdf in setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name="moodlemarking",
     version="1.0.0",
-    description="Scripts for simplifying marking in Moodle",
-    author="Gihan Marasingha§",
+    description="Tools for simplifying marking in Moodle",
+    author="Gihan Marasingha",
     author_email="gihan.marasingha@gmail.com",
     packages=["moodlemarking"],
-    install_requires=["PyPDF2"],
+    install_requires=["pypdf"],
     entry_points={
         "console_scripts": [
             "moodlezip = moodlemarking.moodlezip:main",
