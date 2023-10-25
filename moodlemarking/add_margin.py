e7f319bf81f65fb8758080eabc10df52507677b1
Gihan Marasingha <gihan.marasingha@gmail.com>
2023-10-25 18:33:21 +0100

    add code to control top margin

--- moodlemarking/add_margin.py
+++ moodlemarking/add_margin.py
@@ -3,7 +3,7 @@ import sys
 import argparse
 from pypdf import PdfReader, PdfWriter, PageObject, Transformation
 
-def add_margin(original_file, new_file, margin_width):
+def add_margin(original_file, new_file, margin_width, margin_height):
     reader = PdfReader(original_file)
     writer = PdfWriter()
 
@@ -14,7 +14,8 @@ def add_margin(original_file, new_file, margin_width):
         original_height = page.mediabox[3]
         
         new_width = original_width + margin_width
-        packet = PageObject.create_blank_page(width=new_width, height=original_height)
+        new_height = original_height + margin_height
+        packet = PageObject.create_blank_page(width=new_width, height=new_height)
         
         x_position = new_width - original_width
 
@@ -30,11 +31,12 @@ def main():
     parser = argparse.ArgumentParser(description="Add margins to a PDF file.")
     parser.add_argument("original_file", help="The original PDF file.")
     parser.add_argument("new_file", help="The new PDF file with added margins.")
-    parser.add_argument("-m", "--margin", type=int, default=200, help="Width, in pixels, of the left margin (default is 200).")
+    parser.add_argument("-ml", "--marginleft", type=int, default=200, help="Width, in pixels, of the left margin (default is 200).")
+    parser.add_argument("-mt", "--margintop", type=int, default=0, help="Height, in pixels, of the top margin (default is 0).")
     
     args = parser.parse_args()
 
-    add_margin(args.original_file, args.new_file, args.margin)
+    add_margin(args.original_file, args.new_file, args.marginleft, args.margintop)
 
 if __name__ == "__main__":
     main()
